a7192618b08adeb3de188bca5932de9f93dc241a
PedroHS137 <pjherreras@hotmail.com>
2020-08-27 22:46:08 -0500

    like working

--- tarea_uno/lib/main.dart
+++ tarea_uno/lib/main.dart
@@ -1,76 +1,73 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
-        
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Material App',
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({
+    Key key,
+  }) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
   int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }
-
+  bool _seen = false;
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-       
-        title: Text(widget.title),
+        title: Text('Click the FAB'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.favorite,
+              color: _seen? Colors.red : Colors.grey
+            ),
+            onPressed: () {
+              _counter++;
+              print("$_counter");
+              setState(() {
+                _seen = !_seen;
+              });
+            },
+            color: Colors.grey,
+            // child: Text(
+            //   "click me",
+            //   style: TextStyle(color: Colors.black),
+            // ),
+          )
+        ],
       ),
       body: Center(
-        
         child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Text("count: $_counter"),
+            // MaterialButton(onPressed: () {
+            //   _counter++;
+            //   print("$_counter");
+            //   setState(() {
+
+            //   });
+            // },
+            // color: Colors.blue,
+            // child: Text("click me",
+            // style: TextStyle(color: Colors.black),),)
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
